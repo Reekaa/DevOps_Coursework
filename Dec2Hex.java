691a7f90f2a19f94c031b7f7dbbbcb48221947ab
Reka Forgacs <rekaforgacs@Rekas-MBP.broadband>
2021-11-01 17:54:14 +0000

    adding some error handling to Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,13 +7,24 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        Arg1 = Integer.parseInt(args[0]);
+        if(args.length == 0){
+            System.out.println("Please provide a number to convert.");
+            System.exit(0);
+        }
+
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e){
+            System.out.println("Please provide a valid number");
+            System.exit(0);
+        }
+        
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
+        
         while(num != 0)
         {
             rem=num%16;
@@ -21,8 +32,7 @@ class Dec2Hex
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+        System.out.println("Hexadecimal representation is: " + hexadecimal); 
+         
     }
 }
-
