cd41b77315e3f8bb32e48229ad0b2ad8c049b864
Reekaa <forgacsreka88@gmail.com>
2021-11-06 13:34:27 +0000

    updated unit tests for invalid input

---
+++ src/Dec2Hex.java
@@ -0,0 +1,35 @@
+class Dec2Hex {
+
+    public static int Arg1;
+
+    public static void main(String[] args) {
+
+        // validation check
+        if (args.length <= 0) {
+            System.err.println("Please provide a number to convert.");
+        } else {
+            // parsing int
+            try {
+                Arg1 = Integer.parseInt(args[0]);
+                char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                int rem, num;
+                num = Arg1;
+                String hexadecimal = "";
+                System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+                // convertion
+                while (num != 0) {
+                    rem = num % 16;
+                    hexadecimal = ch[rem] + hexadecimal;
+                    num = num / 16;
+                }
+
+                System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+            } catch (NumberFormatException e) {
+                System.err.println("Please provide a valid number");
+            }
+        }
+    }
+}
+
---
+++ test/Dec2HexTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+/**
+ * Dec2Hex Tester.
+ *
+ * @author Reka Forgacs
+ * @version 1.0
+ * @since <pre>Nov 3, 2021</pre>
+ */
+public class Dec2HexTest {
+    /**
+     * Method: main(String args[])
+     */
+    @Test
+    public void testWithCorrectInput() {
+        Dec2Hex.main(new String[] {"100"});
+    }
+
+    @Test
+    public void testNoInput() {
+        Dec2Hex.main(new String[]{});
+    }
+
+    @Test
+    public void testWithNonIntegerInput() {
+        Dec2Hex.main(new String[] {"sometext"});
+    }
+}
